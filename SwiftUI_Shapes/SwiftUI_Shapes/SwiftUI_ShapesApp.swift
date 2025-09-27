f348078ff37080a52bfedb68511f720fc2d6b1ee
Ambreen Bano <ambreenbano.amu@gmail.com>
2025-09-27 20:19:46 +0530

    iOS : SwiftUI_Shapes

---
+++ SwiftUI_Shapes/SwiftUI_Shapes/SwiftUI_ShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ShapesApp.swift
+//  SwiftUI_Shapes
+//
+//  Created by user2 on 27/09/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ShapesContentView()
+        }
+    }
+}
